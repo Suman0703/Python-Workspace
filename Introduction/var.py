4c13ebc76477acfc7aaac1e2394086f788e791f3
Suman0703 <sumandevimehmi36@gamil.com>
2025-12-07 14:20:54 +0530

    Learning Python Basics

---
+++ Introduction/var.py
@@ -0,0 +1,12 @@
+# Multiple Assignment
+
+a,b = 3,4
+print (a,b)
+
+x = y = z = 100
+print(x,y,z)
+
+# Constants
+PI = 3.14
+GRAVITY = 9.8
+
---
+++ Loops/main.py
@@ -0,0 +1,20 @@
+# loops are used to repeat instructions
+
+#while loop
+count = 1 #iterators
+while count <= 5:
+    print("Hello")
+    count += 1
+print(count)
+
+#another way
+i = 1
+while i<=6:
+    print("Suman")
+    i += 1
+
+# printing numbers
+j = 1
+while j<=6:
+    print(j)
+    j += 1
