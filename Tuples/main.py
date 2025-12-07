601db028a2957a81beafec9897de61f8d60f48a9
Suman0703 <sumandevimehmi36@gamil.com>
2025-12-07 16:56:25 +0530

    Learning Basics of Python

--- Tuples/main.py
+++ Tuples/main.py
@@ -7,6 +7,18 @@ print(type(tup))
 t = (9,)
 print(t)
 
+# Tuple packing
+data = 10, 20, 30
+print(data)
+print(type(data))
+
+# Tuple unpacking 
+dat = (1, 2, 3)
+a, b, c = dat
+print(a)
+print(b)
+print(c)
+
 # Tuple methods
 
 print(tup.index(6)) # returns index of first occurrence
