9a1da035a4d41a6855bf52ee5fcad5a2919415e2
Suman0703 <sumandevimehmi36@gamil.com>
2025-12-08 14:13:04 +0530

    Learning Python Basics

---
+++ Functions/ques_2.py
@@ -0,0 +1,8 @@
+# WAP to print the length of a list 
+name = list(map(str, input("Enter numbers input Values:").split()))
+print (name)
+
+def print_len(name):
+    print(len(name))
+
+print_len(name)
